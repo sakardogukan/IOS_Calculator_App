c09ca7634c9e04f91c5e7739e0c5a8b86f7a266e
DGKN <sakardogukan@gmail.com>
2024-03-25 01:29:59 +0300

    app.js updated

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ const div = document.querySelector(".c4")
 const multiply = document.querySelector(".c8")
 const minus = document.querySelector(".c12")
 const plus = document.querySelector(".c16")
-const dot = document.querySelectorAll(".c18")
+const dot = document.querySelector(".c18")
 const equals = document.querySelector(".c19")
+
+const numArr = [percent.innerText, div.innerText, multiply.innerText, minus.innerText, plus.innerText, dot.innerText, equals.innerText]
+console.log(numArr)
 
 localStorage.setItem("src1", 0)
 localStorage.setItem("src2", 0)
 
-console.log(dot[0].innerText)
 console.log(ac.className)
-console.log(dot);
+console.log(dot)
+console.log(ac.innerText)
+
+let process1 = 0
+let process2 = 0
 
 keys.addEventListener("click", (e) => {
-
+    console.log(e)
     ac.addEventListener("click", (e) => {
         localStorage.setItem("src1", "")
         localStorage.setItem("src2", "")
@@ -33,6 +39,20 @@ keys.addEventListener("click", (e) => {
         screen.innerText += e.target.textContent
         localStorage.setItem("src1", Number(screen.innerText))
     }
+    numArr.forEach((p) => {
+        if (e.target.innerText == p) {
+            console.log(e.target.innerText)
+            process1 = Number(localStorage.getItem("src1"))
+            process2 += process1
+            result.innerText += process2
+            // plusProcess()
+
+        }
+    })
 
 
 })
+
+// function plusProcess() {
+//     result.innerText = process2
+// }
